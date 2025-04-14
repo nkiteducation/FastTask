c913f928ff201fc246c379d261370a9bc7e155da
Koryagin Nazar <iteducation36@gmail.com>
2025-04-14 09:43:34 +0000

    refactor settings structure and add example environment configuration

--- src/core/settings.py
+++ src/core/settings.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from pydantic_settings import (
     BaseSettings,
     SettingsConfigDict,
@@ -7,30 +8,22 @@ from pydantic_settings import (
 )
 from pydantic import BaseModel, Field, ConfigDict
 
-
 class Settings(BaseModel):
     model_config = ConfigDict(frozen=True)
 
-
 class ProjectSettings(Settings):
-    name: str = Field(default="FastTask")
-    version: str = Field(default="1.0.0")
-    description: str = Field(
-        default="""The project is an API for task management
-        (To-Do List) using FastAPI, SQLAlchemy and asynchronous
-        database. It includes functionality for creating, updating,
-        deleting and filtering tasks with support for users and roles."""
-    )
-
+    name: str = "MyProject"
+    version: str = "1.0"
+    description: str = "Project Description"
 
 class UvicornSettings(Settings):
-    port: int = Field(default=8000)
-    host: str = Field(default="127.0.0.1")
-    workers: int = Field(default=1)
+    port: int = 8000
+    host: str = "127.0.0.1"
+    workers: int = 1
 
-
-class Settings(BaseSettings):
-    development: bool = Field(default=False)
+class AppSettings(BaseSettings):
+    development: bool = False
+    
     project: ProjectSettings = ProjectSettings()
     uvicorn: UvicornSettings = UvicornSettings()
 
@@ -43,14 +36,16 @@ class Settings(BaseSettings):
         return (
             DotEnvSettingsSource(
                 settings_cls,
-                ["./src/example.env", "./src/.env"],
+                [
+                    "./src/example.env",
+                    "./src/.env",
+                ],
                 env_file_encoding="utf-8",
                 env_nested_delimiter="_",
             ),
             YamlConfigSettingsSource(settings_cls, "./src/config.yaml"),
             TomlConfigSettingsSource(settings_cls, "./src/config.toml"),
         )
-
-
-config = Settings()
-print(config.project.name)
+        
+config = AppSettings()
+print(config)
