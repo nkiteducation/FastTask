8b9bfbd8afbabc0e81c8304a403d09a220d57f88
Koryagin Nazar <iteducation36@gmail.com>
2025-04-15 07:13:51 +0000

    add SessionManager class for managing database sessions with async support

---
+++ src/database/session.py
@@ -0,0 +1,55 @@
+import asyncio
+from contextlib import asynccontextmanager
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext.asyncio import async_sessionmaker
+from sqlalchemy.ext.asyncio import async_scoped_session
+from loguru import logger
+from core.settings import config
+
+
+class SessionManager:
+    def __init__(
+        self, database_url: str, pool_size: int, max_overflow: int, pool_timeout: int
+    ):
+        self.engine = create_async_engine(
+            database_url,
+            pool_size=pool_size,
+            max_overflow=max_overflow,
+            pool_timeout=pool_timeout,
+            future=True,
+        )
+        self.session_local = async_sessionmaker(
+            bind=self.engine, expire_on_commit=False
+        )
+        self.session = async_scoped_session(
+            self.session_local, scopefunc=asyncio.current_task
+        )
+        logger.info("SessionManager initialized with database URL: {}", database_url)
+
+    @asynccontextmanager
+    async def session_scope(self):
+        session = self.session()
+        logger.info("Session scope started")
+        try:
+            yield session
+            logger.debug("Session scope completed successfully")
+        except Exception as e:
+            await session.rollback()
+            logger.error("Session scope encountered an error: {}", e, exc_info=True)
+            raise e
+        finally:
+            await session.close()
+            await self.session.remove()
+            logger.debug("Session scope closed and session removed")
+
+    async def dispose(self):
+        await self.engine.dispose()
+        logger.info("Engine disposed")
+
+
+sesssion_manager = SessionManager(
+    config.database.URL.url,
+    config.database.poolSize,
+    config.database.maxOverflow,
+    config.database.poolTimeout,
+)
