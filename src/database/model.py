1b2da64f7cbf22507564b3fcf9b94dc04802a16f
Koryagin Nazar <iteducation36@gmail.com>
2025-04-15 17:09:50 +0000

    docs: update README.md to include additional project structure details

--- src/database/model.py
+++ src/database/model.py
@@ -12,6 +12,7 @@ class CoreModel(DeclarativeBase, AsyncAttrs):
         s1 = re.sub(r"([^_])([A-Z])", r"\1-\2", cls.__name__)
         return s1.lower()
 
+
 class User(CoreModel, UUIDMixin, TimestampMixin):
     user_name: Mapped[str] = mapped_column(unique=True)
     email: Mapped[str] = mapped_column(unique=True)
