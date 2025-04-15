aa2eacd2a8bc257ff136ddd02bca6d9bccdf6413
Koryagin Nazar <iteducation36@gmail.com>
2025-04-15 07:08:04 +0000

    refactor settings.py to enhance structure and add URLSettings and DatabaseSettings classes

--- src/core/settings.py
+++ src/core/settings.py
@@ -1,4 +1,4 @@
-from pathlib import Path
+from typing import Optional
 from pydantic_settings import (
     BaseSettings,
     SettingsConfigDict,
@@ -6,26 +6,61 @@ from pydantic_settings import (
     YamlConfigSettingsSource,
     DotEnvSettingsSource,
 )
-from pydantic import BaseModel, Field, ConfigDict
+from pydantic import BaseModel, ConfigDict
+from sqlalchemy import URL
+
 
 class Settings(BaseModel):
     model_config = ConfigDict(frozen=True)
+
 
 class ProjectSettings(Settings):
     name: str = "MyProject"
     version: str = "1.0"
     description: str = "Project Description"
 
+
 class UvicornSettings(Settings):
     port: int = 8000
     host: str = "127.0.0.1"
     workers: int = 1
 
+
+class URLSettings(Settings):
+    driverName: Optional[str] = None
+    userName: Optional[str] = None
+    password: Optional[str] = None
+    host: Optional[str] = None
+    port: Optional[int] = None
+    database: Optional[str] = None
+
+    @property
+    def url(self) -> str:
+        return URL.create(
+            drivername=self.driverName,
+            username=self.userName,
+            password=self.password,
+            host=self.host,
+            port=self.port,
+            database=self.database,
+        )
+
+
+class DatabaseSettings(Settings):
+    echo: bool = False
+    poolSize: int = 10
+    maxOverflow: int = 5
+    poolTimeout: int = 30
+
+    URL: URLSettings = URLSettings()
+
+
 class AppSettings(BaseSettings):
     development: bool = False
-    
+
     project: ProjectSettings = ProjectSettings()
     uvicorn: UvicornSettings = UvicornSettings()
+    database: DatabaseSettings = DatabaseSettings()
 
     model_config = SettingsConfigDict(
         extra="ignore",
@@ -42,9 +77,11 @@ class AppSettings(BaseSettings):
                 ],
                 env_file_encoding="utf-8",
                 env_nested_delimiter="_",
+                env_ignore_empty=True,
             ),
             YamlConfigSettingsSource(settings_cls, "./src/config.yaml"),
             TomlConfigSettingsSource(settings_cls, "./src/config.toml"),
         )
-        
+
+
 config = AppSettings()
