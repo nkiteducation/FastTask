8a034ca8a03f33ed05fb051731860a4e85878ca8
Koryagin Nazar <iteducation36@gmail.com>
2025-04-15 07:22:46 +0000

    add CoreModel class for SQLAlchemy ORM with async support

---
+++ src/database/model.py
@@ -0,0 +1,11 @@
+import re
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import declared_attr
+from sqlalchemy.ext.asyncio import AsyncAttrs
+
+
+class CoreModel(DeclarativeBase, AsyncAttrs):
+    @declared_attr
+    def __tablename__(cls) -> str:
+        s1 = re.sub(r"([^_])([A-Z])", r"\1_\2", cls.__name__)
+        return s1.lower()
