91210028b6f10d80b72241c5c8e4079f25806491
Koryagin Nazar <iteducation36@gmail.com>
2025-04-23 16:52:03 +0000

    refactor: remove unused user_tasks table and update relationships in User and Task models

--- src/database/model.py
+++ src/database/model.py
@@ -2,11 +2,8 @@ import enum
 import re
 
 from datetime import datetime
-from uuid import UUID
 
-import sqlalchemy as sa
-
-from sqlalchemy import Column, DateTime, Enum, ForeignKey, Table, func
+from sqlalchemy import ForeignKey
 from sqlalchemy.ext.asyncio import AsyncAttrs
 from sqlalchemy.orm import (
     DeclarativeBase,
@@ -46,23 +43,27 @@ class Status(enum.Enum):
 
 
 # -------------------- MODELS --------------------
-user_tasks = Table(
-    "user-tasks",
-    CoreModel.metadata,
-    Column("user_id", ForeignKey("user.id"), primary_key=True),
-    Column("task_id", ForeignKey("task.id"), primary_key=True),
-)
-
-
 class User(CoreModel, TimestampMixin, UUIDMixin):
     name: Mapped[str]
     email: Mapped[str]
     password_hesh: Mapped[str]
-    role: Mapped[Role]
+
+    board_links: Mapped[list["BoardInUser"]] = relationship(back_populates="user")
+    boards: Mapped[list["Board"]] = relationship(
+        secondary="board_users", viewonly=True, back_populates="participants"
+    )
+
+
+class Board(CoreModel, TimestampMixin, UUIDMixin):
+    title: Mapped[str]
+
+    user_links: Mapped[list["BoardInUser"]] = relationship(back_populates="board")
+    participants: Mapped[list["User"]] = relationship(
+        secondary="board_users", viewonly=True, back_populates="boards"
+    )
 
     tasks: Mapped[list["Task"]] = relationship(
-        secondary=user_tasks,
-        back_populates="user_tasks",
+        back_populates="board", cascade="all, delete-orphan"
     )
 
 
@@ -73,7 +74,16 @@ class Task(CoreModel, TimestampMixin, UUIDMixin):
     priority: Mapped[Priority] = mapped_column(default=Priority.MEDIUM)
     status: Mapped[Status] = mapped_column(default=Status.TODO)
 
-    user_tasks: Mapped["User"] = relationship(
-        secondary=user_tasks,
-        back_populates="tasks",
-    )
+    board_id: Mapped[int] = mapped_column(ForeignKey("boards.id"))
+    board: Mapped["Board"] = relationship(back_populates="tasks")
+
+
+class BoardInUser(CoreModel):
+    __tablename__ = "board_users"
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), primary_key=True)
+    board_id: Mapped[int] = mapped_column(ForeignKey("boards.id"), primary_key=True)
+    role: Mapped[Role] = mapped_column(default=Role.USER)
+
+    user: Mapped["User"] = relationship(back_populates="board_links")
+    board: Mapped["Board"] = relationship(back_populates="user_links")
