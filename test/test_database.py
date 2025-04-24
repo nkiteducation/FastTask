68c3e2f062bdfee15a921bf92f778d5d5cbdbeca
Koryagin Nazar <iteducation36@gmail.com>
2025-04-24 18:29:33 +0000

    refactor: simplify user database tests by removing unused fixtures and correcting password hash reference

--- test/test_database.py
+++ test/test_database.py
@@ -1,33 +1,17 @@
-import pytest
-import pytest_asyncio
-from database.session import SessionManager
 import faker
-from database.model import Role, Priority, Status, User, Task
+import pytest
 
-
-@pytest.fixture
-def fake_user(fake: faker.Faker):
-    return {
-        "name": fake.name(),
-        "email": fake.email(),
-        "password_hesh": fake.password(),
-        "role": fake.enum(Role),
-    }
-
-
-@pytest.fixture
-def fake_task(fake: faker.Faker):
-    return {
-        "title": fake.sentence(nb_words=6).rstrip("."),
-        "description": fake.paragraph(nb_sentences=3),
-        "deadline": fake.date_time_between(start_date="now", end_date="+30d"),
-        "priority": fake.enum(Priority),
-        "status": fake.enum(Status),
-    }
+from database.model import User
+from database.session import SessionManager
 
 
 @pytest.mark.asyncio
-async def test_user(fake_user, session_manager: SessionManager):
+async def test_user_db(fake: faker.Faker, session_manager: SessionManager):
+    fake_user = {
+        "name": fake.name(),
+        "email": fake.email(),
+        "password_hash": fake.password(),
+    }
     async with session_manager.session_scope() as session:
         user = User(**fake_user)
         session.add(user)
@@ -36,36 +20,4 @@ async def test_user(fake_user, session_manager: SessionManager):
 
     assert user.name == fake_user["name"]
     assert user.email == fake_user["email"]
-    assert user.password_hesh == fake_user["password_hesh"]
-    assert user.role == fake_user["role"]
-
-
-@pytest.mark.asyncio
-async def test_task(fake_task, session_manager: SessionManager):
-    async with session_manager.session_scope() as session:
-        task = Task(**fake_task)
-        session.add(task)
-        await session.commit()
-        await session.refresh(task)
-
-    assert task.title == fake_task["title"]
-    assert task.description == fake_task["description"]
-    assert task.deadline == fake_task["deadline"]
-    assert task.priority == fake_task["priority"]
-    assert task.status == fake_task["status"]
-
-
-@pytest.mark.asyncio
-async def test_user_tasks(fake_user, fake_task, session_manager: SessionManager):
-    user = User(**fake_user)
-
-    for _ in range(10):
-        user.tasks.append(Task(**fake_task))
-
-    async with session_manager.session_scope() as session:
-        session.add(user)
-        await session.commit()
-        await session.refresh(user)
-        rows = [t for t in await user.awaitable_attrs.tasks]
-
-    assert 10 == len(rows)
+    assert user.password_hash == fake_user["password_hash"]
