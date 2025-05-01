1c99be497c1c74d205d8c4a93be0c86b8594a521
Koryagin Nazar <iteducation36@gmail.com>
2025-05-01 10:06:48 +0000

    refactor: update TokenData model and change decode_jwt return type for improved type safety

--- src/api/v1/auth/utils.py
+++ src/api/v1/auth/utils.py
@@ -5,6 +5,7 @@ from datetime import datetime, timedelta, timezone
 import jwt
 
 from passlib.context import CryptContext
+from pydantic import BaseModel, EmailStr
 
 from core.settings import config
 
@@ -14,6 +15,15 @@ _PUBLIC_KEY = config.jwt.public_key_path.read_text()
 _ALGORITHM = config.jwt.algorithm
 _ISSUER = config.project.name
 _AUDIENCE = "my-api"
+
+class TokenData(BaseModel):
+    sub: str
+    name: str
+    email: EmailStr
+    exp: datetime
+    iss: str
+    aud: str
+    jti: str
 
 def get_password_hash(password: str) -> str:
     return pwd_context.hash(password)
@@ -33,11 +43,12 @@ def encode_jwt(payload: dict, expire_delta: timedelta = timedelta(minutes=15)) -
     }
     return jwt.encode(to_encode, key=_PRIVATE_KEY, algorithm=_ALGORITHM)
 
-def decode_jwt(token: str) -> dict:
-        return jwt.decode(
+def decode_jwt(token: str) -> TokenData:
+        payload = jwt.decode(
             jwt=token,
             key=_PUBLIC_KEY,
             algorithms=[_ALGORITHM],
             audience=_AUDIENCE,
             issuer=_ISSUER,
         )
+        return TokenData.model_validate(payload)
