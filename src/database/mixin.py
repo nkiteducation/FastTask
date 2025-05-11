d71b306514d140d79b379cd12f5c47cb83ffa8ec
Koryagin Nazar <iteducation36@gmail.com>
2025-05-11 08:01:02 +0000

    refactor: add index to UUIDMixin id column for improved query performance

--- src/database/mixin.py
+++ src/database/mixin.py
@@ -7,7 +7,9 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 
 class UUIDMixin:
-    id: Mapped[uuid.UUID] = mapped_column(primary_key=True, default=uuid.uuid4)
+    id: Mapped[uuid.UUID] = mapped_column(
+        primary_key=True, default=uuid.uuid4, index=True
+    )
 
 
 class TimestampMixin:
