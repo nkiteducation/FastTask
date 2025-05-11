cec4507b819f0cc907f46174b2b95ad01205733f
Koryagin Nazar <iteducation36@gmail.com>
2025-05-11 11:28:22 +0000

    feat: enhance JWT token generation by adding factory function and updating imports

--- src/api/v1/auth/views_utils.py
+++ src/api/v1/auth/views_utils.py
@@ -1,10 +1,16 @@
+from datetime import timedelta
+from typing import Callable
+
 from fastapi import HTTPException, status
 from pydantic import EmailStr, SecretStr
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from api.v1.auth.utils import password_hash
+from api.v1.auth.utils import encode_jwt, password_hash
+from core.settings import config
 from database.model import User
+
+TOKEN_TYPE_FIELD = "type"
 
 
 async def create_user_in_the_database(
@@ -30,3 +36,19 @@ async def generation_registration_error(user, session: AsyncSession):
         status_code=status.HTTP_400_BAD_REQUEST,
         detail=" and ".join(detail),
     )
+
+
+def jwt_factory(
+    token_type: str,
+    lifetime: timedelta,
+) -> Callable[[dict[str, any]], str]:
+    def create_token(payload: dict[str, any]) -> str:
+        data = payload.copy()
+        data[TOKEN_TYPE_FIELD] = token_type
+        return encode_jwt(data, lifetime)
+
+    return create_token
+
+
+get_access_token = jwt_factory("access", config.jwt.access_token_lifetime)
+get_refresh_token = jwt_factory("refresh", config.jwt.refresh_token_lifetime)
