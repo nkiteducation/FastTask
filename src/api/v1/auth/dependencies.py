f5f46b2c908f1b4693197dfb1953d6e56a7fec57
Koryagin Nazar <iteducation36@gmail.com>
2025-05-11 11:29:13 +0000

    feat: add user authentication validation function and update imports

--- src/api/v1/auth/dependencies.py
+++ src/api/v1/auth/dependencies.py
@@ -1,9 +1,14 @@
 from typing import Annotated
 
-from fastapi import Form
+from fastapi import Depends, Form, HTTPException, status
 from pydantic import EmailStr, SecretStr
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
-from api.v1.shemas import UserCreate
+from api.v1.auth.utils import verify_password
+from api.v1.shemas import UserCreate, UserDTO
+from database.model import User
+from database.session import session_manager
 
 
 def get_form_user(
@@ -12,3 +17,20 @@ def get_form_user(
     password: Annotated[SecretStr, Form()],
 ):
     return UserCreate(name=name, email=email, password=password)
+
+
+async def validate_auth_user(
+    email: Annotated[str, Form()],
+    password: Annotated[str, Form()],
+    session: AsyncSession = Depends(session_manager.session_scope),
+) -> UserDTO:
+    unauthed_exc = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid username or password",
+    )
+
+    user = await session.scalar(select(User).where(User.email == email))
+    if not user or not verify_password(password, user.password_hash):
+        raise unauthed_exc
+
+    return UserDTO.model_validate(user)
