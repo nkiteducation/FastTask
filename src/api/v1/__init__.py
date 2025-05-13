7645f50178ee706902c9576cbd1161df4358b6f9
Koryagin Nazar <iteducation36@gmail.com>
2025-05-13 20:44:46 +0000

    feat: add secured router with JWT verification for v1 API

--- src/api/v1/__init__.py
+++ src/api/v1/__init__.py
@@ -1,6 +1,10 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
-from api.v1.auth.views import router as auth_router
+from api.v1.auth.dependencies import verification_access_jwt
+from api.v1.auth.endpoint import router as auth_router
 
 v1_router = APIRouter(prefix="/v1")
+secured_router = APIRouter(dependencies=Depends(verification_access_jwt))
+
 v1_router.include_router(auth_router)
+v1_router.include_router(secured_router)
