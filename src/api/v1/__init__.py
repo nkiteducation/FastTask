9aeb69d70315669c43d9d0c825683d6a6ec69ea3
Koryagin Nazar <iteducation36@gmail.com>
2025-05-15 12:07:28 +0000

    feat: include room router in secured API router

--- src/api/v1/__init__.py
+++ src/api/v1/__init__.py
@@ -2,9 +2,11 @@ from fastapi import APIRouter, Depends
 
 from api.v1.auth.dependencies import verification_access_jwt
 from api.v1.auth.endpoint import router as auth_router
+from api.v1.rooms.endpoint import router as room_router
 
 v1_router = APIRouter(prefix="/v1")
 secured_router = APIRouter(dependencies=Depends(verification_access_jwt))
 
 v1_router.include_router(auth_router)
 v1_router.include_router(secured_router)
+secured_router.include_router(room_router)
