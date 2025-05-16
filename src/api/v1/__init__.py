a83e66f2bb64f5c2a0f25cbe0eecc6f44ae31935
Koryagin Nazar <iteducation36@gmail.com>
2025-05-16 09:21:53 +0000

    refactor: update secured router to include board router and remove room router

--- src/api/v1/__init__.py
+++ src/api/v1/__init__.py
@@ -2,11 +2,11 @@ from fastapi import APIRouter, Depends
 
 from api.v1.auth.dependencies import verification_access_jwt
 from api.v1.auth.endpoint import router as auth_router
-from api.v1.rooms.endpoint import router as room_router
+from api.v1.board.endpoint import router as board_router
 
 v1_router = APIRouter(prefix="/v1")
-secured_router = APIRouter(dependencies=Depends(verification_access_jwt))
+secured_router = APIRouter(dependencies=[Depends(verification_access_jwt)])
 
+secured_router.include_router(board_router)
 v1_router.include_router(auth_router)
 v1_router.include_router(secured_router)
-secured_router.include_router(room_router)
