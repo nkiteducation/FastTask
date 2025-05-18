814da0de162cf6b2fa8501d4d31e9243552866bf
Koryagin Nazar <iteducation36@gmail.com>
2025-05-18 15:36:36 +0000

    refactor: remove secured router and streamline API router structure

--- src/api/v1/__init__.py
+++ src/api/v1/__init__.py
@@ -1,12 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter
 
-from api.v1.auth.dependencies import verification_access_jwt
 from api.v1.auth.endpoint import router as auth_router
 from api.v1.board.endpoint import router as board_router
 
 v1_router = APIRouter(prefix="/v1")
-secured_router = APIRouter(dependencies=[Depends(verification_access_jwt)])
 
-secured_router.include_router(board_router)
+v1_router.include_router(board_router)
 v1_router.include_router(auth_router)
-v1_router.include_router(secured_router)
